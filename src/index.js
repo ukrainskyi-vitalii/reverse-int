7e3e5132488008d8dd262fe6bf831435d9795f55
Vitalii <ukrainskiy.vitalii@gmail.com>
2022-01-04 21:06:02 +0200

    feat: add reverse method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const numberString = n.toString();
+    let i = numberString.length;
+    let result = '';
+    while (i > 0) {
+        let key = i - 1;
+        result = `${numberString[key]}${result}`;
+    }
+    return Number(result);
 }
